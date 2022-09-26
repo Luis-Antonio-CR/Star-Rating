22efbb363b50932688d30656c900cc4c444fa005
Luis-Antonio Castaneda <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-09-26 15:51:08 +0200

    Use of useState for decide witch stars are selected

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -18,9 +18,27 @@ const Star = ({ selected = false }) => (
 
 const createArray = length => [...Array(length)];
 
-const StarRating = ( {totalStars = 5} ) => {
+const StarRating2 = ( {totalStars = 5} ) => {
     
     return createArray(totalStars).map((n,i) => <Star key={i} /> );
+}
+
+const StarRating = ( {totalStars = 5} ) => {
+
+    const [selectedStars] = useState(3);
+    
+    return(
+        <>
+            {createArray(totalStars).map((n,i) => 
+            ( <Star key={i} selected = {selectedStars > i} /> ) )}
+
+            <p>
+                {selectedStars} of {totalStars} stars
+            </p>
+        </>
+    ) ;
+    
+    
 }
 
 export default StarRating;
